83674e2fd92c5c3d6f34b0ddd72b937bcf234e2b
jeevandza <jeevandza4@gmail.com>
2024-03-05 21:49:15 +0530

    added error validations and custom errors

--- src/error_handlers.rs
+++ src/error_handlers.rs
@@ -26,8 +26,28 @@ impl fmt::Display for CustomError {
     }
 }
 
-impl  {
-    
+impl From<DieselError> for CustomError {
+    fn from(error: DieselError) -> CustomError {
+        match error {
+            DieselError::DatabaseError(_, err) => CustomError::new(409, err.message().to_string()),
+            DieselError::NotFound => {
+                CustomError::new(404, "The employee record not found".to_string())
+            }
+            err => CustomError::new(500, format!("Unknown Diesel error: {}", err)),
+        }
+    }
 }
 
-
+impl ResponseError for CustomError {
+    fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        let status_code = match StatusCode::from_u16(self.error_status_code) {
+            Ok(status_code) => status_code,
+            Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+        let error_message = match status_code.as_u16() < 500 {
+            true => self.error_message.clone(),
+            false => "Internal server error".to_string(),
+        };
+        HttpResponse::build(status_code).json(json!({"message": error_message}))
+    }
+}
